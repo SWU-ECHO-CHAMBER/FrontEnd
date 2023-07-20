20f506e6e409d754804212e1ebf5e833ba895271
AlwaysFighting <zosejiri@gmail.com>
2023-07-21 03:24:41 +0900

    Feat: MyUser Page NavigationItem + HomeNav item location change

--- EchoChamber/EchoChamber/Scene/MyPage/MyPageViewController.swift
+++ EchoChamber/EchoChamber/Scene/MyPage/MyPageViewController.swift
@@ -9,8 +9,58 @@ import UIKit
 
 class MyPageViewController : UIViewController {
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .white
+        
+        self.setupNavigationController()
     }
+}
+
+// MARK: - Navigation
+
+private extension MyPageViewController {
+    func setupNavigationController() {
+        
+        let leftFixedSpace = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        leftFixedSpace.width = 36.0
+        
+        let rightFixedSpace = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        rightFixedSpace.width = 20.0
+        
+        if let originalImage = UIImage(named: "Logo") {
+            
+            let imageSize = CGSize(width: 89, height: 39)
+            let resizedImage = originalImage.resize(to: imageSize)
+            
+            let imageView = UIImageView(image: resizedImage)
+            imageView.frame = CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height)
+        
+            let logoButton = UIBarButtonItem(customView: imageView)
+            
+            navigationItem.leftBarButtonItems = [leftFixedSpace, logoButton]
+        }
+        
+        let logoutButton = UIBarButtonItem(image: UIImage(systemName: "rectangle.portrait.and.arrow.right"), style: .plain, target: self, action: nil)
+        
+        let settingButton = UIBarButtonItem(image: UIImage(systemName: "gearshape"), style: .plain, target: self, action: #selector(didTapStettingButton))
+        
+        navigationItem.rightBarButtonItems = [rightFixedSpace, settingButton, logoutButton]
+    }
+    
+}
+
+// MARK: - @objc
+
+private extension MyPageViewController {
+    
+    @objc func didTapLogoutButton() {
+        print("Logout")
+    }
+    
+    @objc func didTapStettingButton() {
+        print("Setting")
+    }
+
 }
